cc4a468b6af0cc5a397e0daa504b35377355cfe6
ojitos369 <ojitos369@gmail.com>
2021-05-18 21:40:50 -0500

    probando conexiones

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="entrada encuesta">
+            <a href="./html/encuesta1.php"> Encuesta de Entrada </a>
+        </div>
+        <div class="salida encuesta">
+            Encuesta de Salida
+        </div>
+    </div>
+</body>
+</html>
