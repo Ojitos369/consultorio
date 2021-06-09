73c73905b2ac24abd94305e10d338940d5114950
ojitos369 <ojitos369@gmail.com>
2021-06-09 14:21:38 -0500

    conexion correcta

--- php/conexion.php
+++ php/conexion.php
@@ -1,17 +1,18 @@
 <?php
 function conectar(){
-    $datos = json_decode("./datos.json");
+    $data = @file_get_contents('datos.json');
+    $datos = json_decode($data, true);
+    
     $user = $datos["username"];
     $password = $datos["passwd"];
     $server = $datos["host"];
     $database = $datos["database"];
     $conexion = new mysqli($server, $user, $password);
     if($conexion->connect_errno){
-        echo $conexion->connect_errno;
+        echo $conexion->connect_errno,"error";
         exit();
     }else{
         echo "Conectado correctamente";
-        echo $datos["username"];
     }
     mysqli_set_charset($conexion,'utf8');
     $db = mysqli_select_db($conexion, $database);
