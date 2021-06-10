5ff7112278e97baa2b0834b0d3a43816731fc345
ojitos369 <ojitos369@gmail.com>
2021-06-09 20:28:44 -0500

    conexion correcta a la base de datos

--- html/encuesta1.php
+++ html/encuesta1.php
@@ -1,9 +1,9 @@
 <?php
-include '../php/conexion.php';
-$conexion = conectar();
+include ('../php/conexion.php');
+$conexion = conectar('../php/datos.json');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,5 +11,13 @@ $conexion = conectar();
     <title>Document</title>
 </head>
 <body>
+<?php
+    $sql = "SELECT pregunta FROM preguntas WHERE id_seccion = 1";
+    $resultado = $conexion->query($sql);
+    while($consulta = $resultado->fetch_array()){
+        echo "<p> $consulta[0] </p>";
+    }
+    $conexion->close();
+?>
 </body>
 </html>
--- php/conexion.php
+++ php/conexion.php
@@ -1,23 +1,21 @@
 <?php
-function conectar(){
-    $data = @file_get_contents('datos.json');
+function conectar($ruta){
+    $data = @file_get_contents($ruta);
     $datos = json_decode($data, true);
-    
     $user = $datos["username"];
     $password = $datos["passwd"];
     $server = $datos["host"];
     $database = $datos["database"];
-    $conexion = new mysqli($server, $user, $password);
+    $conexion = new mysqli($server, $user, $password, $database);
+    #$conexion = mysqli_connect($server, $user, $password, $database);
     if($conexion->connect_errno){
         echo $conexion->connect_errno,"error";
         exit();
-    }else{
-        echo "Conectado correctamente";
-    }
+    }#else{
+        #echo "Conectado correctamente";
+    #}
     mysqli_set_charset($conexion,'utf8');
-    $db = mysqli_select_db($conexion, $database);
-    return $db;
+    return $conexion;
 }
-
-conectar()
+conectar('datos.json')
 ?>
