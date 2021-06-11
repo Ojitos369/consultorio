bf7ee2978d83d2cd15ac1d9e7b976d30121be77e
ojitos369 <ojitos369@gmail.com>
2021-06-11 14:48:37 -0500

    conexion base con node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(__dirname+'/src'));
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/src/index.html'));
+});
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log('Running at port 3000');
