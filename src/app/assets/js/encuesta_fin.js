a9305aa823d7bea433d9b3424144f3064289956e
ojitos369 <ojitos369@gmail.com>
2021-06-21 18:30:48 -0500

    cambio en encuesta final

---
+++ src/app/assets/js/encuesta_fin.js
@@ -0,0 +1,26 @@
+function cambio(clase){
+    let clase_id = parseInt(clase)
+    if (clase_id == 17){
+        let pregunta1 = document.getElementById(String(clase_id));
+        let pregunta2 = document.getElementById(String(clase_id - 1));
+        let pregunta3 = document.getElementById(String(clase_id + 1));
+        pregunta1.style.display = 'none';
+        pregunta2.style.display = 'none';
+        pregunta3.style.display = 'block';
+    }
+    else if (clase_id == 18){
+        let pregunta_Actual = document.getElementById(String(clase_id));
+        let pregunta_Siguiente = document.getElementById(String(clase_id + 1));
+        pregunta_Actual.style.display = 'none';
+        pregunta_Siguiente.style.display = 'block';
+        let enviar = document.getElementById('enviar');
+        enviar.style.display = 'block';
+    }
+    else{
+        let pregunta_Actual = document.getElementById(String(clase_id));
+        let pregunta_Siguiente = document.getElementById(String(clase_id + 1));
+        pregunta_Actual.style.display = 'none';
+        pregunta_Siguiente.style.display = 'block';
+    }
+    //19 maximo
+}
