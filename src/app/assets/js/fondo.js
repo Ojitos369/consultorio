284a8f1097b644dbbab1d7ef3dc7225741e7ddbd
ojitos369 <ojitos369@gmail.com>
2021-06-22 15:46:47 -0500

    arreglo en fondo protector de index

--- src/app/assets/js/fondo.js
+++ src/app/assets/js/fondo.js
@@ -1,10 +1,19 @@
 function ocultar(){
-    let fondo = document.getElementById('protector');
-    fondo.style.display = 'none';
+    let fondo = document.getElementById('img_fondo');
+    let protector = document.getElementById('protector');
+    fondo.style.zIndex = -1000
+    protector.style.display = 'none';
     setTimeout(()=>{
-        let fondo = document.getElementById('protector');
-        fondo.style.display = 'flex';
+        let fondo = document.getElementById('img_fondo');
+        let protector = document.getElementById('protector');
+        fondo.style.zIndex = 9
+        protector.style.display = 'flex';
     },10000);
+}
+
+function inicio(){
+    let fondo = document.getElementById('img_fondo');
+    fondo.style.zIndex = 9
 }
 
 function mostrar_formulario(){
