da8b95de6a115d9625e5eb29f6039c67e476e177
ojitos369 <ojitos369@gmail.com>
2021-08-21 13:07:51 -0500

    flecha anterior

--- js/encuesta_inicial.js
+++ js/encuesta_inicial.js
@@ -1,3 +1,5 @@
+const { exceptions } = require("winston");
+
 function siguiente(numero){
     let id = 'cont_'+numero;
     let sig = 'siguiente_'+numero;
@@ -82,64 +84,65 @@ function siguiente(numero){
 
 function anterior(numero){
     let id = 'cont_'+numero;
+    let sig = 'siguiente_'+numero;
     let ant = 'anterior_'+numero;
-    let input = 'input_'+numero;
+    let sig_act = null
+    try{
+        sig_act = document.getElementById(sig);
+    }catch(error){console.log('No siguiente')}
     let ant_act = document.getElementById(ant);
     let elemento = document.getElementById(id);
-    let valor = document.getElementsByName(input)[0];
-    //console.log(valor);
-    valor = valor.value;
     let enviar = document.getElementById('enviar');
-    //console.log(`pregunta: ${numero} - valor: ${valor}`)
-    if(valor == '' || valor == undefined){
-        alert('El campo no puede ir vacio')
-    }else{
-        let anterior = null
-        let ant_ant = null
-        if(parseInt(numero)<fin){
-            let id_anterior = 'cont_'+String(parseInt(numero) - 1);
-            let ant_ant = 'siguiente_'+String(parseInt(numero) - 1);
-            siguiente = document.getElementById(id_anterior);
-            ant_ant = document.getElementById(ant_ant);
-        }
-        if(anterior){
-            let final = false;
-            if(parseInt(numero)-11 == fin){
-                final = true
-                enviar.classList.add('animacion_abajo');
+
+    let anterior = null
+    let sig_ant = null
+    let ant_ant = null
+
+    if(parseInt(numero)>inicio){
+        let id_anterior = 'cont_'+String(parseInt(numero) - 1);
+        sig_ant = 'siguiente_'+String(parseInt(numero) - 1);
+        try{
+            ant_ant = 'anterior_'+String(parseInt(numero) - 1);
+        }catch(error){ print('No anterior') }
+        anterior = document.getElementById(id_anterior);
+        siguiente_ant = document.getElementById(sig_ant);
+        try{
+            anterior_ant = document.getElementById(ant_ant);
+        }catch(error){ print('No anterior') }
+    }
+
+    if(anterior){
+        let fin = false;
+            if(parseInt(numero) == fin){
+                fin = true
+                enviar.style.display = 'none';
             }
-            elemento.classList.add('animacion_arriba');
-            //ant_act.classList.add('animacion_arriba');
+            elemento.classList.add('animacion_derecha');
+            //siguiente_act.classList.add('animacion_arriba');
             setTimeout(() => {
                 elemento.style.display = 'none';
+                try{
+                    sig_act.style.display = 'none';
+                }catch(error){console.log('No siguiente')}
                 ant_act.style.display = 'none';
-                siguiente.classList.add('animacion_abajo');
-                //ant_ant.classList.add('animacion_abajo');
-                siguiente.style.display = 'block';
-                if(!final){
-                    ant_ant.style.display = 'block';
-                }
-                elemento.classList.remove('animacion_arriba');
-                //ant_act.classList.remove('animacion_arriba');
+                siguiente_ant.style.display = 'block';
+                try{
+                    anterior_ant.style.display = 'block';
+                }catch(error){console.log('No anterior')}
+
+                anterior.classList.add('animacion_izquierda');
+                anterior.style.display = 'block';
+
+
+                elemento.classList.remove('animacion_derecha');
                 setTimeout(() => {
-                    if(final){
-                        enviar.style.display = 'block';
-                        enviar.classList.remove('animacion_abajo');
-                    }
-                    siguiente.classList.remove('animacion_abajo');
-                    //ant_ant.classList.remove('animacion_abajo');
+
+                    anterior.classList.remove('animacion_izquierda');
+                    //anterior.classList.remove('animacion_abajo');
+                    //siguiente_sig.classList.remove('animacion_abajo');
                 }, 1000);
             }, 1000);
-        }
     }
-    let opiniones = document.getElementsByClassName("opinion")
-    for(i=0; i<opiniones.length; i++){
-        opiniones[i].style.height = "150px"
-    }
-    let ele = document.getElementById("cuantos");
-    let cont = document.getElementById("cuantos_cont");
-    ele.style.display = 'none';
-    cont.style.display = 'none';
 }
 
 function leerOpciones(id){
